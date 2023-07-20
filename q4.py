ffaf884f9a9300e85688607713841f0a2bfc9d18
ERIKA FELICIO DA HORA <123699531+Erika492@users.noreply.github.com>
2023-07-20 14:39:12 -0500

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,18 @@
+#split
+#processo de destruturação
+[nome, sobrenome] = "ola mundo".split(" ")
+print(f'{nome}-{sobrenome}')
+
+#replace
+print('\n\n\n\n')
+string = 'Hellu wurd'
+print(string)
+print(string.replace("u", "o"))
+print(string.replace("ur", "ol"))
+
+
+print("HELO WORD".lower())
+print("hello word".upper())
+print("HELlO WORD".title())
+
+print(ord('a'))
